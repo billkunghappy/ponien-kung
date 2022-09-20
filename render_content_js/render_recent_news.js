e121b894238b983723df60d125d77f47a3e32e63
billkunghappy <billkung.happy@gmail.com>
2022-09-19 20:41:15 -0700

    fix recent-news duplicate errors. Update my status to UCLA.

--- render_content_js/render_recent_news.js
+++ render_content_js/render_recent_news.js
@@ -1,9 +1,9 @@
 function render_news(news) {
     var all_icons = ["bullhorn", "paper", "code", "star"];
-    var class_prefix=".example-recent-news-";
+    var class_prefix="example-recent-news-";
     if (all_icons.includes(news.icon)){
         var class_name = class_prefix+news.icon;
-        var news_item=$(class_name).clone().removeClass(class_name);
+        var news_item=$("."+class_name).clone().removeClass(class_name);
         news_item.children("h4").children(".recent-news-title").html(news.title);
         news_item.children(".recent-news-date").html(news.date);
         news_item.attr("href", news.link);
@@ -17,7 +17,7 @@ function render_news(news) {
 $(document).ready(function(){
     $.getJSON("content/recent-news.json", function(data){
         for (let index = 1; index < data.length; index++) {
-            render_news(data[index])
+            render_news(data[index]);
         }
         // console.log(data[0]); // Prints: 14
     }).fail(function(){
